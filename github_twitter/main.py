b67afd6bebd7d4491754ebc9fe5c0cd97e8db420
Pierre Rochard <pierre.rochard@axial.net>
2017-09-13 08:20:08 -0400

    debugging the cron job

--- github_twitter/main.py
+++ github_twitter/main.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+import sys
+sys.path.insert(0, '../')
+
 from github_twitter.scripts.get_pull_requests import insert_response
 from github_twitter.scripts.send_tweet import send_tweet
 
