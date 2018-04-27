f6546c19714a91364ea07daa93fbe08c857c129d
Pierre Rochard <pierre@rochard.org>
2018-04-27 15:18:57 -0400

    default secret

--- src/bitcoin_acks/webapp/settings.py
+++ src/bitcoin_acks/webapp/settings.py
@@ -1,7 +1,15 @@
 import os
+import random
+import string
+
+
+def generate_secret():
+    alphanumeric = string.ascii_uppercase + string.ascii_lowercase + string.digits
+    x = ''.join(random.choice(alphanumeric) for _ in range(32))
+    return x
 
 
 class Config(object):
     DEBUG = False
     FLASK_ADMIN_FLUID_LAYOUT = True
-    SECRET_KEY = os.environ['SECRET_KEY']
+    SECRET_KEY = os.environ.get('SECRET_KEY', default=generate_secret())
