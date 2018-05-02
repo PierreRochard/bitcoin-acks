fa240f9b2cf1280480f6f95c888b902f0758ba36
Pierre Rochard <pierre@rochard.org>
2018-05-02 11:20:10 -0400

    enable html formatting on the details body, make it a modal

--- src/bitcoin_acks/webapp/formatters.py
+++ src/bitcoin_acks/webapp/formatters.py
@@ -21,7 +21,7 @@ def line_count_formatter(view, context, model, name):
 def body_formatter(view, context, model, name):
     if 'details' in context.name:
         return Markup(model.body)
-    full_text = display_text = Markup.escape(model.body)
+    full_text = display_text = Markup.striptags(model.body)
     max_length = 200
     if len(full_text) > max_length:
         display_text = full_text[0:max_length]
