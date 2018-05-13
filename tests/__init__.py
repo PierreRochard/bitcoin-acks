963208583ccc695cad1876b4b5d50c81b34bca12
Pierre Rochard <pierre@rochard.org>
2018-05-13 15:14:16 -0400

    refactor tests to use marshmallow for schema validation

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,5 +3,5 @@ import os
 tests_directory = os.path.dirname(os.path.realpath(__file__))
 fixtures_data_directory = os.path.join(tests_directory, 'fixtures_data')
 issues_file_path = os.path.join(fixtures_data_directory, 'issues.json')
-pull_requests_file_path = os.path.join(fixtures_data_directory, 'pull_requests.json')
+pull_requests_file_path = os.path.join(fixtures_data_directory, 'pull_requests_{state}_{newest_first}_{limit}.json')
 pull_request_file_path = os.path.join(fixtures_data_directory, 'pull_request.json')
