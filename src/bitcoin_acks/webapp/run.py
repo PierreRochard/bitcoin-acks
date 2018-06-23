abd6cc93e892d8cf837ceb2a102df37308fb1575
Pierre Rochard <pierre@rochard.org>
2018-06-23 07:47:30 -0400

    Switch to flask-sqla to avoid needing teardown boilerplate
    http://flask.pocoo.org/docs/1.0/patterns/sqlalchemy/

--- src/bitcoin_acks/webapp/run.py
+++ src/bitcoin_acks/webapp/run.py
@@ -1,9 +1,8 @@
 from flask import Flask, request, Response
 from flask_admin import Admin
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
+from flask_sqlalchemy import SQLAlchemy
 
-from bitcoin_acks.database.session import session_scope, get_url
+from bitcoin_acks.database.session import session_scope
 from bitcoin_acks.models import PullRequests, Logs
 from bitcoin_acks.webapp.views import PullRequestsModelView
 
@@ -12,6 +11,7 @@ def create_app(config_object: str):
     app = Flask(__name__)
 
     app.config.from_object(config_object)
+    db = SQLAlchemy(app)
 
     @app.after_request
     def after_request(response):
@@ -32,20 +32,11 @@ def create_app(config_object: str):
             log_session.add(record)
         return response
 
-    pg_url = get_url()
-    engine = create_engine(pg_url, connect_args={'sslmode': 'prefer'})
-    Session = scoped_session(sessionmaker(bind=engine))
-    session = Session()
-
     admin = Admin(app,
                   name='Bitcoin ACKs',
                   template_mode='bootstrap3',
                   url='/',
-                  index_view=PullRequestsModelView(PullRequests, session))
-
-    @app.teardown_appcontext
-    def shutdown_session(exception=None):
-        Session.remove()
+                  index_view=PullRequestsModelView(PullRequests, db.session))
 
     @app.route('/robots.txt')
     def robots_txt():
--- src/bitcoin_acks/webapp/settings.py
+++ src/bitcoin_acks/webapp/settings.py
@@ -2,6 +2,8 @@ import os
 import random
 import string
 
+from bitcoin_acks.database.session import get_url
+
 
 def generate_secret():
     alphanumeric = string.ascii_uppercase + string.ascii_lowercase + string.digits
@@ -13,3 +15,4 @@ class Config(object):
     DEBUG = False
     FLASK_ADMIN_FLUID_LAYOUT = True
     SECRET_KEY = os.environ.get('SECRET_KEY', default=generate_secret())
+    SQLALCHEMY_DATABASE_URI = get_url()
