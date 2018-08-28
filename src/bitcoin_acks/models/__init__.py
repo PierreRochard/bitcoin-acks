ce31bfd39b5589c36e1df116376e84857fb0c5a5
Wladimir J. van der Laan <laanwj@gmail.com>
2018-08-28 18:13:59 +0200

    Add tooting capability

--- src/bitcoin_acks/models/__init__.py
+++ src/bitcoin_acks/models/__init__.py
@@ -7,4 +7,5 @@ from .pull_requests import PullRequests
 from .pull_requests_labels import PullRequestsLabels
 from .repositories import Repositories
 from .tweets import Tweets
+from .toots import Toots
 from .users import Users
