d753b3f26aeee7e39ef304196a281ddf0ca472db
Pierre Rochard <pierre@rochard.org>
2020-06-21 14:56:52 -0500

    Fix tweet sending

--- src/bitcoin_acks/scripts/send_tweet.py
+++ src/bitcoin_acks/scripts/send_tweet.py
@@ -9,7 +9,7 @@ from bitcoin_acks.models import PullRequests
 from bitcoin_acks.models.tweets import Tweets
 
 
-def send_tweet():
+def send_tweet(pull_request_number=None):
     with session_scope() as session:
         now = datetime.datetime.utcnow()
         yesterday = now - datetime.timedelta(days=1)
@@ -18,15 +18,23 @@ def send_tweet():
                           os.environ['BTC_TWITTER_OAUTH_TOKEN'],
                           os.environ['BTC_TWITTER_OAUTH_TOKEN_SECRET']
                           )
-        next_pull_request = (
-            session
-                .query(PullRequests)
-                .order_by(PullRequests.merged_at.asc())
-                .filter(PullRequests.merged_at > yesterday)
-                .filter(PullRequests.tweet_id.is_(None))
-                .filter(PullRequests.merged_at.isnot(None))
+        if pull_request_number:
+            next_pull_request = (
+                session
+                    .query(PullRequests)
+                    .filter(PullRequests.number == pull_request_number)
                 .first()
-        )
+            )
+        else:
+            next_pull_request = (
+                session
+                    .query(PullRequests)
+                    .order_by(PullRequests.merged_at.asc())
+                    .filter(PullRequests.merged_at > yesterday)
+                    .filter(PullRequests.tweet_id.is_(None))
+                    .filter(PullRequests.merged_at.isnot(None))
+                    .first()
+            )
         if next_pull_request is None:
             return
         commits_url = 'https://api.github.com/repos/bitcoin/bitcoin/commits'
@@ -43,12 +51,12 @@ def send_tweet():
             status = '''
             {0}'s first merged PR: {1}
             Congratulations!  🎉🍾🎆
-            '''.format(next_pull_request.author,
-                       next_pull_request.url)
+            '''.format(next_pull_request.author.name,
+                       next_pull_request.html_url)
         tweet = twitter.update_status(status=status)
         new_tweet = Tweets()
         new_tweet.id = tweet['id']
-        new_tweet.pull_request_id = next_pull_request.id
+        new_tweet.pull_request_id = next_pull_request.number
         session.add(new_tweet)
         next_pull_request.tweet_id = tweet['id']
 
