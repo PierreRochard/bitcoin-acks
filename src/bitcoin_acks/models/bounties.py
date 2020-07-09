bc50613ac557db6146ef931b7b774ad3f58f77f2
Pierre Rochard <pierre@rochard.org>
2020-07-08 21:21:16 -0500

    First iteration on bounties model

---
+++ src/bitcoin_acks/models/bounties.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from bitcoin_acks.database.base import Base
+from bitcoin_acks.models.users import Users
+
+
+class Bounties(Base):
+    __tablename__ = 'bounties'
+
+    id = Column(String, primary_key=True)
+
+    amount = Column(Integer)
+
+    published_at = Column(DateTime, nullable=False)
+
+    pull_request_id = Column(String,
+                             ForeignKey('pull_requests.id'),
+                             nullable=False)
+    creator_id = Column(String)
+
+    creator = relationship(Users,
+                           primaryjoin=creator_id == Users.id,
+                           foreign_keys='[Bounties.creator_id]',
+                           backref='bounties'
+                           )
